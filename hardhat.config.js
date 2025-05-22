c1ea9f6bfcafe7e4e5dd782486e05f57b795de94
Dikansha Bindal <146829994+DikanshaBindal@users.noreply.github.com>
2025-05-22 12:25:32 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,7 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 
 const { PrivateKey } = require('./secret.json');
 
@@ -11,20 +10,19 @@ module.exports = {
    defaultNetwork: 'testnet',
 
    networks: {
-      hardhat: {
-      },
+      hardhat: {},
       testnet: {
-         url: 'https://rpc.test.btcs.network',
-         accounts: [PrivateKey],
-         chainId: 1115,  // Ensure this matches the Core testnet chain ID
+         url: 'https://rpc.test2.btcs.network',
+         accounts: ['e0a951788665661e6e049e6adbf8d732de612898e5cba335cc64bd27ad8c661d'], // Use your private key from secret.json
+         chainId: 1114, // Core Testnet2 chain ID
       }
    },
    solidity: {
       compilers: [
         {
-           version: '0.8.21',  // Update to at least 0.8.20 to support the 'paris' EVM
+           version: '0.8.24', // Latest supported by Core
            settings: {
-              evmVersion: 'paris',  // Specify 'paris' EVM version
+              evmVersion: 'shanghai', // Use 'shanghai' for Testnet2
               optimizer: {
                  enabled: true,
                  runs: 200,
@@ -39,6 +37,6 @@ module.exports = {
       artifacts: './artifacts',
    },
    mocha: {
-      timeout: 20000,  // You can adjust the timeout as needed
+      timeout: 20000,
    },
 };
